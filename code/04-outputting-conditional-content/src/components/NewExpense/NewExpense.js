ea4bc39abcde42e9c365f04b0803fadce6f71d55
moritzHauptmann <mohauptmann@gmail.com>
2022-07-16 18:30:53 +1000

    finishing section 5

--- code/04-outputting-conditional-content/src/components/NewExpense/NewExpense.js
+++ code/04-outputting-conditional-content/src/components/NewExpense/NewExpense.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [formOpen, setFormOpen] = useState(false);
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
   };
 
+  const formOpenHandler = () => {
+    setFormOpen(!formOpen);
+  };
+
+  const addExpenseButton = (
+    <div>
+      <button type="submit" onClick={formOpenHandler}>
+        Add Expense
+      </button>
+    </div>
+  );
+
   return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    <div className="new-expense">
+      {formOpen ? (
+        <ExpenseForm
+          onFormSubmit={formOpenHandler}
+          onSaveExpenseData={saveExpenseDataHandler}
+        />
+      ) : (
+        addExpenseButton
+      )}
     </div>
   );
 };
